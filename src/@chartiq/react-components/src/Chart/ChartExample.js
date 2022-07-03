34c4edfb4d80084107d6b61ad4cf92af350a0a47
Sonyl Nagale <sonyl@nagale.com>
2022-07-03 12:13:47 -0400

    Cleanup and enable FDC3

--- src/@chartiq/react-components/src/Chart/ChartExample.js
+++ src/@chartiq/react-components/src/Chart/ChartExample.js
@@ -20,18 +20,20 @@ import "chartiq/examples/translations/translationSample";
 import "chartiq/examples/markers/tradeAnalyticsSample";
 import "chartiq/examples/markers/videoSample";
 
+import registerFDC3 from "chartiq/js/extras/fdc3";
+
 export { CIQ };
 
 // Callback function where you can access both the chartEngine and the UIContext.
 const chartInitialized = ({ chartEngine, uiContext }) => {
 	// Assign stx and CIQ to window for development convenience
 	Object.assign(window, { stx: chartEngine, CIQ });
+
+	registerFDC3(chartEngine)
 };
 
 // Callback to execute when chart is loaded for first time
-const onChartReady = (chartEngine) => {
-	console.log('ready')
-};
+const onChartReady = (chartEngine) => { };
 
 const exampleResources = {
 	quoteFeed: quoteFeedSimulator,
